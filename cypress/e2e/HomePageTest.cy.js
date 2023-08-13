5fd2e4be268a66ba0c7408d1d46a568f0b841317
shifat124 <shifatlaskor779@gmail.com>
2023-08-14 01:26:06 +0600

    commit of 14 august, 2023

--- cypress/e2e/HomePageTest.cy.js
+++ cypress/e2e/HomePageTest.cy.js
@@ -1,19 +1,65 @@
 describe('Home Page Test', () => {
-    it('Validate the count of items', () => {
-       cy.visit('https://www.saucedemo.com/v1/');
-       cy.get('#user-name').type('standard_user');
-       cy.get('#password').type('secret_sauce');
-       cy.get('#login-button').click();
-       cy.get('.inventory_item').each((element, index, list) => {
-        // This callback will be executed for each element in the list
-        // You can perform any actions on the element if needed
-      }).then((elements) => {
-        // The elements array contains the list of elements
-        const count = elements.length;
-        
-        // Now you can use the 'count' variable to validate
-        cy.wrap(count).should('eq', 6); // Assert that the count is equal to 6
-      });
-      });
-     
+  it('Validate the count of items', () => {
+    cy.visit('https://www.saucedemo.com/v1/');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.get('.inventory_item').each((element, index, list) => {
+      // This callback will be executed for each element in the list
+      // You can perform any actions on the element if needed
+    }).then((elements) => {
+      // The elements array contains the list of elements
+      const count = elements.length;
+      // Now you can use the 'count' variable to validate
+      cy.wrap(count).should('eq', 6); // Assert that the count is equal to 6
     });
+  });
+  it('Validate sauce labs backpack item click', () => {
+    cy.visit('https://www.saucedemo.com/v1/');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.get('#item_4_title_link').click();
+    cy.get('.inventory_details_name').invoke('text').should('equal', 'Sauce Labs Backpack');
+  });
+  it('Validate sauce labs bike light item click', () => {
+    cy.visit('https://www.saucedemo.com/v1/');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.get('#item_0_title_link').click();
+    cy.get('.inventory_details_name').invoke('text').should('equal', 'Sauce Labs Bike Light');
+  });
+  it('Validate sauce labs bolt t-shirt item click', () => {
+    cy.visit('https://www.saucedemo.com/v1/');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.get('#item_1_title_link').click();
+    cy.get('.inventory_details_name').invoke('text').should('equal', 'Sauce Labs Bolt T-Shirt');
+  });
+  it('Validate sauce labs fleece jacket item click', () => {
+    cy.visit('https://www.saucedemo.com/v1/');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.get('#item_5_title_link').click();
+    cy.get('.inventory_details_name').invoke('text').should('equal', 'Sauce Labs Fleece Jacket');
+  });
+  it('Validate sauce labs onesie item click', () => {
+    cy.visit('https://www.saucedemo.com/v1/');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.get('#item_2_title_link').click();
+    cy.get('.inventory_details_name').invoke('text').should('equal', 'Sauce Labs Onesie');
+  });
+  it('Validate test.allTheThings() t-shirt (red) item click', () => {
+    cy.visit('https://www.saucedemo.com/v1/');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.get('#item_3_title_link').click();
+    cy.get('.inventory_details_name').invoke('text').should('equal', 'Test.allTheThings() T-Shirt (Red)');
+  });
+});
--- cypress/e2e/LogInPageTest.cy.js
+++ cypress/e2e/LogInPageTest.cy.js
@@ -1,52 +1,46 @@
 describe('LogIn Page Test', () => {
-    it('Validate LogIn with valid username and password - Positive Testing', () => {
-       cy.visit('https://www.saucedemo.com/v1/');
-       cy.get('#user-name').type('standard_user');
-       cy.get('#password').type('secret_sauce');
-       cy.get('#login-button').click();
-       cy.url().should('eq', 'https://www.saucedemo.com/v1/inventory.html');
-      });
-    it('Validate LogIn with invalid username and password - Negative Testing', () => {
-       cy.visit('https://www.saucedemo.com/v1/');
-       cy.get('#user-name').type('sta_usr');
-       cy.get('#password').type('sec_sau');
-       cy.get('#login-button').click();
-       cy.url().should('eq', 'https://www.saucedemo.com/v1/inventory.html');
-      });
-
-     it('Validate LogIn by reading usernames and passwords from the JSON file', () => {
-      cy.fixture('LogInData.json').then((data) => {
-        const usernames = data.users.map((user) => user.username);
-        const passwords = data.users.map((user) => user.password);
-
-          // Iterate through usernames and passwords
-    for (let i = 0; i < usernames.length; i++) {
-       cy.visit('https://www.saucedemo.com/v1/');
-       cy.get('#user-name').type(usernames[i]);
-       cy.get('#password').type(passwords[i]);
-       cy.get('#login-button').click();
-      // Retrieve the URL and save it in a variable
-      cy.url().then(url => {
-      const currentUrl = url
-      if(currentUrl === "https://www.saucedemo.com/v1/inventory.html"){
-      cy.log('LogIn Passed');
-      cy.url().should('eq', 'https://www.saucedemo.com/v1/inventory.html');
-  }
-    else{
-      cy.log('LogIn Failed');
-      cy.get("h3[data-test='error']").should('be.visible');
-
-    
-  }
-});
-  }
-  
-});
-});
+  it('Validate LogIn with valid username and password - Positive Testing', () => {
+    cy.visit('https://www.saucedemo.com/v1/');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.url().should('eq', 'https://www.saucedemo.com/v1/inventory.html');
+  });
+  it('Validate LogIn with invalid username and password - Negative Testing', () => {
+    cy.visit('https://www.saucedemo.com/v1/');
+    cy.get('#user-name').type('sta_usr');
+    cy.get('#password').type('sec_sau');
+    cy.get('#login-button').click();
+    cy.url().should('eq', 'https://www.saucedemo.com/v1/inventory.html');
+  });
+  it('Validate LogIn by reading usernames and passwords from the JSON file', () => {
+    cy.fixture('LogInData.json').then((data) => {
+      const usernames = data.users.map((user) => user.username);
+      const passwords = data.users.map((user) => user.password);
+      // Iterate through usernames and passwords
+      for (let i = 0; i < usernames.length; i++) {
+        cy.visit('https://www.saucedemo.com/v1/');
+        cy.get('#user-name').type(usernames[i]);
+        cy.get('#password').type(passwords[i]);
+        cy.get('#login-button').click();
+        // Retrieve the URL and save it in a variable
+        cy.url().then(url => {
+          const currentUrl = url
+          if (currentUrl === "https://www.saucedemo.com/v1/inventory.html") {
+            cy.log('LogIn Passed');
+            cy.url().should('eq', 'https://www.saucedemo.com/v1/inventory.html');
+          }
+          else {
+            cy.log('LogIn Failed');
+            cy.get("h3[data-test='error']").should('be.visible');
+          }
+        });
+      }
+    });
+  });
 });
 
 
 
 
 
-     
