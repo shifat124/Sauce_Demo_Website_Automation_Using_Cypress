1b29e636e63b533bd8e0da6f6d491beda7555859
shifat124 <shifatlaskor779@gmail.com>
2023-08-24 23:45:54 +0600

    final project updated

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   pageLoadTimeout: 200000,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
